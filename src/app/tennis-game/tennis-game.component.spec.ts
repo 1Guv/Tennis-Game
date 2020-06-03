b93f2ceaf0c3e3d59782b5b68fe5dfd272c181a6
Guv <Guv@thehouseshop.com>
2020-06-04 00:59:32 +0100

    added some tests

--- src/app/tennis-game/tennis-game.component.spec.ts
+++ src/app/tennis-game/tennis-game.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { TennisGameComponent } from './tennis-game.component';
+import { TennisGameComponent, PlayerCLass } from './tennis-game.component';
 
 describe('TennisGameComponent', () => {
   let component: TennisGameComponent;
@@ -22,4 +21,18 @@ describe('TennisGameComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(TennisGameComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Tennis Score');
+  });
+
+  it('should give one number', () => {
+    const result = component.getRandomOneOrZero();
+    console.log('should give one number', result);
+    expect(result.toString().length).toBe(1, 'unexpected random number result');
+  });
+
 });
